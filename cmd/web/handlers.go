4c2de3fb38c5a71c41386ac24845c1c07723c99c
jesse-kroon <jessekroon2@gmail.com>
2026-04-28 16:33:50 +0200

    add templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,13 +10,21 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	template, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	template, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = template.Execute(w, nil)
+
+	err = template.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
